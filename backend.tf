974d4be03429f12d7d1a3f2c739a0ce41a02edfe
archana <dhagearchana14@gmail.com>
2023-06-10 16:43:19 +0530

    2nd commit

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-archu"
+    bucket = "mydev-tf-state-bucket-archu1"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table1"
