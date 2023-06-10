c0617e1ec6eb7a03f6e27103814fa971b6e594d2
archana-star <127712051+archana-star@users.noreply.github.com>
2023-06-10 18:15:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "jenkins-sgroup-dec-2021" 
+    default     = "sg-06ac06ab507b7d054" 
 }
 
 variable "tag_name" { 
